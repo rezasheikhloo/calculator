c803f424d58e064584c78ee543790728536c4872
rezasheikhloo <m.rezasheikhloo@gmail.com>
2024-08-17 16:19:20 +0330

    5.27

--- calculator.py
+++ calculator.py
@@ -5,4 +5,8 @@ def typing (event):
     if key == "=":
 result.set(round(eval(result.get())), 2)
     else:
-        if len(result)
+        if len(result.get())< 8:
+            result.set(result.get()+key)
+
+ def mouse_enter(event):
+    event.widget.config(background="lightblue", foreground="black")
